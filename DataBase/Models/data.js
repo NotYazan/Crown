ae9e21d4089ba1da4e02885ca3e5263c2f839ec5
Yazan <73472040+NotYazan@users.noreply.github.com>
2023-01-13 09:03:45 +0200

    Update data.js

--- DataBase/Models/data.js
+++ DataBase/Models/data.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const data = mongoose.Schema({
-
     GuildId : String,
-    OwnerID : String,
-    ServerName : String,
 });
 
 let BlackList = mongoose.model('blacklist', data);
